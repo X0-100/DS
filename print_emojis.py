4bc9bce126a75ade3b72a7da87d265cdfed9a2ac
X0-100 <saurabhit39swhattheycallme@yahoo.com>
2018-12-21 14:39:56 +0530

    C:\Users\saurabh.sanyal\KANYE-superpower\print_emojis.py

--- print_emojis.py
+++ print_emojis.py
@@ -1,7 +1,43 @@
 '''
 This file will print emoji
+Unicodes are
+\U0001f600
 
 '''
 
-def emoji():
-    pass
+'''
+using unicodes
+'''
+
+def grinning_face():
+    print("\U0001f600")
+
+def grinning_squiting_face(): #Printing on terminal
+    print("\U0001F606")
+
+def rofl():
+    print("\U0001F923")
+
+'''
+using CLDR short Names
+'''
+
+def CLDR_grinning_face():
+    print('\N{grinning face}')
+
+def CLDR_slightly_smiling_face():
+    print('\N{slightly smiling face}')
+
+def CLDR_winking_face(): #printing on terminal
+    print('\N{winking face}')
+
+
+
+
+if __name__ == '__main__':
+    grinning_face()
+    grinning_squiting_face()
+    rofl()
+    CLDR_grinning_face()
+    CLDR_slightly_smiling_face()
+    CLDR_winking_face()
