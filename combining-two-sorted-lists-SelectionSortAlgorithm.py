eded41e68460940ef4266bce777a617fe3867844
X <saurabhit39swhattheycallme@yahoo.com>
2019-01-02 22:03:59 +0530

    C:\Users\X\KANYE-superpower\KANYE-superpower\combining-two-sorted-lists-SelectionSortAlgorithm.py

---
+++ combining-two-sorted-lists-SelectionSortAlgorithm.py
@@ -0,0 +1,36 @@
+'''
+This file combines two sorted lists using Bubble Sort Algorithm
+'''
+def combine():
+    test_list1 = [1,5,6,9,11]
+    test_list2 = [3,4,7,8,10]
+    emp_li = []
+    for i in test_list1:
+        emp_li.append(i)
+    for j in test_list2:
+        emp_li.append(j)
+
+    print('Resultant List is {} '.format(emp_li))
+
+    '''
+    #TEST DATA :
+    emp_li = [5,2,6,7,3]
+    '''
+    emp_li = [105,120,10,200,20]
+    '''
+    Sorting emp_li by implementing Selection Sort Algorithm
+    '''
+    pass = 1
+
+    sortedsublist = []
+    unsortedsublist = [105,120,10,200,20]
+
+    for i in range(len(unsortedsublist)):
+        while(i = )
+        if(unsortedsublist[i]<unsortedsublist[i+1]):
+            sortedsublist.append(unsortedsublist[i])
+
+
+
+if __name__ == '__main__':
+    combine()
