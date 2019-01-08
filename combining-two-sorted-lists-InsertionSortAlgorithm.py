740023c7c5e0034eb17076c76ec6c6e99ccb20be
X0-100 <saurabhit39swhattheycallme@yahoo.com>
2019-01-08 17:38:55 +0530

    .

--- combining-two-sorted-lists-InsertionSortAlgorithm.py
+++ combining-two-sorted-lists-InsertionSortAlgorithm.py
@@ -2,19 +2,25 @@
 This implements combining two sorted lists using Insertion Sort Algorithm
 '''
 def sort(li):
-    for i in range(1,len(li)):
-        if(li[i] < li[i-1]):
-            #Swapping
-            iMax = li[i]
-            iMin = li[i-1]
-            li[i-1] = iMax
-            li[i] = iMin
+    print('pointer {} '.format(pointer))
+    print(li)
+    pointer = 1
 
-
+    for i in range(1,len(li)-1):
+        pointer = pointer + 1
+        print('pointer {} '.format(pointer))
+        for j in range(i-pointer, i):
+            if(li[i] < li[i-1]):
+                #Swapping
+                iMax = li[i]
+                iMin = li[i-1]
+                li[i-1] = iMax
+                li[i] = iMin
+                print(li)
 
 def combine():
     #print('Hello World!')
-    emp_li = [6,5,3,1,8,7,2,4]
+    emp_li = [12,3,1,5,8]
     return(emp_li)
 
 
