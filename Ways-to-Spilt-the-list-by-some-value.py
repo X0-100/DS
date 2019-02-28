517362a6674c2fa96d4090dc11103921fe4de4ab
X0-100 <saurabhit39swhattheycallme@yahoo.com>
2019-02-28 15:37:04 +0530

    Commited

---
+++ Ways-to-Spilt-the-list-by-some-value.py
@@ -0,0 +1,37 @@
+#https://www.geeksforgeeks.org/python-ways-to-spilt-the-list-by-some-value/
+
+
+list0 = []
+li1 = []
+li2 = []
+	
+
+def insert_list():
+    number = 5
+    for x in range(0, number):
+        list0.append(raw_input('Enter in List     '))
+      
+
+
+def split_inhalves():
+    count = 0
+    for x in range(0, len(list0)):
+        if count < 1:
+            li1.append(list0[x])
+            count = count + 1
+        else:
+            li2.append(list0[x])
+    print('li1 -- {} '.format(li1))
+    print('li2 -- {} '.format(li2))
+    
+
+
+
+if __name__ == '__main__':
+    insert_list()
+    split_inhalves()
+
+
+
+
+
