d1f6b2ebb55066fcef93e686f0966df3d6e70d89
X0-100 <saurabhit39swhattheycallme@yahoo.com>
2019-04-09 11:15:52 +0530

    Finished

--- Split-dictionary-keys-and-values-into-separate-lists.py
+++ Split-dictionary-keys-and-values-into-separate-lists.py
@@ -1,40 +1,41 @@
-#	intial_dictionary {'a': 'akshat', 'b': 'bhuvan', 'c': 'chandan'}
-#	keys :  dict_keys(['a', 'b', 'c'])
-#	values :  dict_values(['akshat', 'bhuvan', 'chandan'])
+#       intial_dictionary {'a': 'akshat', 'b': 'bhuvan', 'c': 'chandan'}
+#       keys :  dict_keys(['a', 'b', 'c'])
+#       values :  dict_values(['akshat', 'bhuvan', 'chandan'])
+#       finished implementation
 
 
 class App:
-	initial_dictionary  = {}
-	keys = []
-	values = []
+        initial_dictionary  = {}
+        keys = []
+        values = []
 
 
-	def __init__(self,initial_dictionary, keys, values):
-		self.initial_dictionary = initial_dictionary
-		self.keys = keys
-		self.values = values
+        def __init__(self,initial_dictionary, keys, values):
+                self.initial_dictionary = initial_dictionary
+                self.keys = keys
+                self.values = values
 
-	
-	def process_keys(self):
-		for key in self.initial_dictionary:
-			self.keys.append(key)
-	
-	def print_keys(self):
-		print 'keys : dict_keys({})'.format(self.keys)
-		
+        
+        def process_keys(self):
+                for key in self.initial_dictionary:
+                        self.keys.append(key)
+        
+        def print_keys(self):
+                print('keys : dict_keys({})'.format(self.keys))
+                
 
-	def process_values(self):
-    		for key in self.initial_dictionary:
-        		self.values.append(self.initial_dictionary[key])  
+        def process_values(self):
+                for key in self.initial_dictionary:
+                    self.values.append(self.initial_dictionary[key])  
 
-	def print_values(self):
-		print 'values : dict_values({})'.format(self.values)
-    			
+        def print_values(self):
+                print('values : dict_values({})'.format(self.values))
+                        
       
 if __name__ == '__main__':
-	app = App({'a': 'akshat', 'b': 'bhuvan', 'c': 'chandan'},[],[])
-	app.process_keys()
-	app.print_keys()
-	app.process_values()
-	app.print_values()
-	
+        app = App({'a': 'akshat', 'b': 'bhuvan', 'c': 'chandan'},[],[])
+        app.process_keys()
+        app.print_keys()
+        app.process_values()
+        app.print_values()
+        
