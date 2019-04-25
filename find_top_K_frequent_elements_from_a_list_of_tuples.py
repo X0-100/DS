e0a680fce3c0b818508d561f292fe1bc2262bfaf
X <saurabhit39swhattheycallme@yahoo.com>
2019-04-25 23:36:44 +0530

    Unfinished

--- find_top_K_frequent_elements_from_a_list_of_tuples.py
+++ find_top_K_frequent_elements_from_a_list_of_tuples.py
@@ -3,7 +3,7 @@
 # DESC(2) - Given a list of tuples with word as first element and its frequency as second element, the task is to find top k frequent element.
 # DESC(3) - Initial List of tuple is [[(‘Name’, 151)], [(‘ACe’, 400)], [(‘TURN’, 210)], [(‘RED’, 1113)], [(‘YELLOW’, 1)]]
 # DESC(4) - Top ‘K’ elements are [(‘RED’, 1113), (‘ACe’, 400), (‘TURN’, 210)]
-# Unfinished
+# Unfinished--
 
 import sys
 
