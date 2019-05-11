a9aad01463c7f0605ab59d4ed918eeface036f4e
X <saurabhit39swhattheycallme@yahoo.com>
2019-05-11 10:29:27 +0530

    Finished

---
+++ get_last_n_elements_from_given_list.py
@@ -0,0 +1,17 @@
+# Get last N elements from given list
+
+# test_list = [4,5,2,6,7,8,10]
+
+
+def process():
+    test_list = [4,5,2,6,7,8,10]
+
+    n = 5
+
+    test_list = test_list[-n:]
+
+    print(test_list)
+
+
+if __name__ == '__main__':
+    process()
